720683774378e31fa3d73d1db78a937e82f7bd7c
O'mil <87229937+O-mil@users.noreply.github.com>
2023-02-28 13:21:19 +0900

    [Bronze III] Title: 최소, 최대, Time: 1728 ms, Memory: 318164 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+
+        for ( int i = 0; i < N; i++) {
+            int a = sc.nextInt();
+            arr[i] = a;
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for ( int i = 1; i < N; i++) {
+            
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+
+            if ( arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        sc.close();
+        System.out.printf("%d %d\n", min, max);
+    }
+}
