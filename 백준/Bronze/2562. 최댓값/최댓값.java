8092cd116a9dd247202c83154ddff9f2ca3a6ed0
O'mil <87229937+O-mil@users.noreply.github.com>
2023-02-28 13:22:06 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a;
+        int[] arr = new int[9];
+
+        for (int i = 0; i < 9; i++) {
+            a = sc.nextInt();
+            arr[i] = a;
+        }
+
+        int max = arr[0];
+        int temp = 1;
+
+        for ( int i = 1; i < 9; i++) {
+            if ( arr[i] > max) {
+                max = arr[i];
+                temp = i + 1;
+            }
+        }
+        sc.close();
+        System.out.println(max);
+        System.out.println(temp);
+
+    }
+}
