c5503830f236a0b0eca49b39eb5df07a397accdd
O'mil <87229937+O-mil@users.noreply.github.com>
2023-02-28 16:38:04 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 208 ms, Memory: 17604 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int[] student = new int[31];
+        int[] student = new int[31];
 
-		for (int i = 1; i < 29; i++) {
-			int success = sc.nextInt();
-			student[success] = 1;
-		}
-		for (int i = 1; i < student.length; i++) {
-			if (student[i] != 1)
-				System.out.println(i);
-		}
-
-		sc.close();
-	}
+        for ( int i = 1; i < 29; i++) {
+            int sucess = sc.nextInt();
+            student[sucess] = 1;
+        }
+        for ( int i = 1; i < student.length; i++) {
+            if ( student[i] != 1) {
+                System.out.println(i);
+            }
+        }
+        sc.close();
+    }
 }
