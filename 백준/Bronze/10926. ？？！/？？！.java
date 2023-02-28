217ce010b98599150ddb9af12480cafe99122c94
O'mil <87229937+O-mil@users.noreply.github.com>
2023-02-28 12:27:52 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        
+        String A = in.nextLine();
+        
+        System.out.print(A + "??!");
+        
+        in.close();
+    }
+}
