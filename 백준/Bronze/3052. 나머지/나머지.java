908d7247ecf8bc6c50b6273ebbc58b074cf09801
O'mil <87229937+O-mil@users.noreply.github.com>
2023-03-14 11:16:14 +0900

    [Bronze II] Title: 나머지, Time: 200 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        boolean bl;
+        int cnt = 0;
+
+        for ( int i = 0; i < arr.length; i++) {
+            int A = sc.nextInt();
+            arr[i] = A % 42;
+        }
+
+        for ( int i = 0; i < arr.length; i++) {
+            bl = false;
+            for ( int j = i + 1; j < arr.length; j++) {
+                if ( arr[i] == arr[j]) {
+                    bl = true;
+                    break;
+                }
+            }
+            if ( bl == false) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+
+        sc.close();
+    }
+}
