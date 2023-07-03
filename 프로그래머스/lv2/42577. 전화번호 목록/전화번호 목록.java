5e3715d9a6a66def1cd8c27ee5415ef0c05105f2
O'mil <87229937+O-mil@users.noreply.github.com>
2023-07-04 01:24:27 +0900

    [level 2] Title: 전화번호 목록, Time: 396.85 ms, Memory: 212 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,13 +1,23 @@
 import java.util.*;
+
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        Arrays.sort(phone_book);
         
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) return false;
+        // 1. HashMap 선언
+        Map<String, Integer> map = new HashMap<>();
+        
+        // 2. 전화번호를 HashMap에 삽입
+        for (int i = 0; i < phone_book.length; i++) {
+            map.put(phone_book[i], i);
         }
         
+        // 3. 전화번호의 substring이 HashMap에 있는지 확인한다.
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                if (map.containsKey(phone_book[i].substring(0, j))) answer = false;
+            }
+        }
         return answer;
     }
 }
