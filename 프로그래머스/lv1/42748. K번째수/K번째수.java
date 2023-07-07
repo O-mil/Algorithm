e638dc426d9df513efad8f41e00ea0b0d1e05f19
O'mil <87229937+O-mil@users.noreply.github.com>
2023-07-08 06:02:22 +0900

    [level 1] Title: K번째수, Time: 0.35 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for (int i = 0; i < commands.length; i++) {
+            int start = commands[i][0];
+            int end = commands[i][1];
+            int num = commands[i][2];
+            
+            int[] arr = new int[end - start + 1];
+            for (int j = 0; j < arr.length; j++) {
+                arr[j] = array[start - 1 + j];
+            }
+            Arrays.sort(arr);
+            answer[i] = arr[num - 1];
+        }
+        return answer;
+    }
+}
