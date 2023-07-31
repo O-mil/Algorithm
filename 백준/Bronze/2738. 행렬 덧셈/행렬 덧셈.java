8cf4351a718fe95e22fb40a23fe3d945140c1ac9
O'mil <ghkwjd5343@gmail.com>
2023-08-01 02:18:39 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 664 ms, Memory: 36540 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int cnt = 0;
+
+        int[][] arr = new int[n][m];
+
+        while (cnt != 2) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    arr[i][j] += sc.nextInt();
+                }
+            }
+            cnt++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
