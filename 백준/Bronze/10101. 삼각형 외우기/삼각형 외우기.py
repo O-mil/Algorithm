5288343ae212815871848134878170977acd495f
O'mil <ghkwjd5343@gmail.com>
2023-11-14 19:42:46 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.py
@@ -0,0 +1,16 @@
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+
+if (n1 + n2 + n3 == 180):
+    
+    if (n1 == 60 and n2 == 60 and n3 == 60):
+        print("Equilateral")
+    
+    elif (n1 == n2 or n2 == n3 or n1 == n3):
+        print("Isosceles")
+    
+    elif (n1 != n2 and n2 != n3 and n1 != n3):
+        print("Scalene")
+else:
+    print("Error")
