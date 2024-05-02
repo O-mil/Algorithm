2975c092ed7cbbea1b2e202149f30fc690ebdab3
O'mil <ghkwjd5343@gmail.com>
2024-05-02 20:46:42 +0900

    [level 2] Title: 프로세스, Time: 0.52 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int num: priorities) {
+            queue.offer(num);
+        }
+        
+        int answer = 0;
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (queue.peek() == priorities[i]) {
+                    queue.poll();
+                    answer++;
+                    
+                    if (location == i) {
+                        return answer;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
