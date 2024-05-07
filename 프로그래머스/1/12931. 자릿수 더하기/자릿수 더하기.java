630e0d1e5a0dc9aae1f19a1c8d04f56394cc9928
O'mil <ghkwjd5343@gmail.com>
2024-05-07 20:17:11 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
+        
         int answer = 0;
-
         while (n > 0) {
             answer += n % 10;
-            n = n/10;
+            n /= 10;
         }
-
+        
         return answer;
     }
 }
