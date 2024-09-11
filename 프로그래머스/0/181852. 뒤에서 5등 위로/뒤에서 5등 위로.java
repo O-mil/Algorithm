47a72083244d6d9fb2b503b72f62ff576baca1eb
O'mil <ghkwjd5343@gmail.com>
2024-09-11 11:03:46 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.49 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -4,11 +4,8 @@ class Solution {
     public int[] solution(int[] num_list) {
         Arrays.sort(num_list);
         
-        int[] answer = new int[num_list.length - 5];
+        int[] answer = Arrays.copyOfRange(num_list, 5, num_list.length);
         
-        for (int i = 5; i < num_list.length; i++) {
-            answer[i - 5] = num_list[i];
-        }
         return answer;
     }
 }
