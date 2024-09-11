4b2168178ceb2e3babdab065e3003a69b9b7d7e8
O'mil <ghkwjd5343@gmail.com>
2024-09-11 11:33:56 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -2,9 +2,7 @@ class Solution {
     public int solution(String myString, String pat) {
         
         myString = myString.replace('A', 'X').replace('B', 'A').replace('X', 'B');
-        
-        if (myString.contains(pat)) return 1;
-        
-        return 0;
+                
+        return myString.contains(pat) ? 1 : 0;
     }
 }
