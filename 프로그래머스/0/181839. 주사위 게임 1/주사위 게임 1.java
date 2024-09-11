6ac1c41cc017441aae51b9ca4d26c1455252ea32
O'mil <ghkwjd5343@gmail.com>
2024-09-11 11:16:16 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.06 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int a, int b) {
         
-        if (a % 2 == 0 && b % 2 == 0) return Math.max(a, b) - Math.min(a, b);
+        if (a % 2 == 0 && b % 2 == 0) return Math.abs(a - b);
         else if (a % 2 == 0 || b % 2 == 0) return 2 * (a + b);
         else return (a * a) + (b * b);
     }
