fa880142a78fcfe2ed2fe9951ba8f98fe58d8075
O'mil <ghkwjd5343@gmail.com>
2024-09-13 09:48:02 +0900

    [level 0] Title: 가위 바위 보, Time: 0.52 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,12 +1,6 @@
 class Solution {
     public String solution(String rsp) {
-        String answer = "";
-        
-        for (int i = 0; i < rsp.length(); i++) {
-            if (rsp.substring(i, i+1).equals("2")) answer += "0";
-            else if (rsp.substring(i, i+1).equals("0")) answer += "5";
-            else answer += "2";
-        }
-        return answer;
+       
+        return rsp.replaceAll("2", "1").replaceAll("5", "2").replaceAll("0", "5").replaceAll("1", "0");
     }
 }
