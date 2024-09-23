43c4c76d4d5187fcde21f6508f8b51774b8cc6d9
O'mil <ghkwjd5343@gmail.com>
2024-09-23 14:37:41 +0900

    [level 0] Title: 수 조작하기 2, Time: 1102.76 ms, Memory: 390 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -3,10 +3,12 @@ class Solution {
         String answer = "";
         
         for (int i = 0; i < numLog.length - 1; i++) {
-            if (numLog[i+1] - numLog[i] == 1) answer += 'w';
-            else if (numLog[i+1] - numLog[i] == -1) answer += 's';
-            else if (numLog[i+1] - numLog[i] == 10) answer += 'd';
-            else answer += 'a';
+            switch(numLog[i+1] - numLog[i]) {
+                case 1 : answer += 'w'; break;
+                case -1 : answer += 's'; break;
+                case 10 : answer += 'd'; break;
+                case -10: answer += 'a'; break;
+            }
         }
         return answer;
     }
